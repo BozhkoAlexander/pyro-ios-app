603e399adb4890d25deaedd5f72795c32eefcb61
Alexander Bozhko <alexander.bozhko@filmgrail.com>
2024-04-25 13:19:23 +0300

    added ios app

---
+++ ios-pyro/ios-pyro/ios_pyroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_pyroApp.swift
+//  ios-pyro
+//
+//  Created by Alexander Bozhko on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_pyroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
